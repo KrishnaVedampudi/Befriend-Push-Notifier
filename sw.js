af9ff7386a7ae5846083003e59dbb1e4bdb44e15
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-11-10 11:58:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,33 +7,67 @@ OneSignal.push(function() {
     },
     subdomainName: "befriend",
     welcomeNotification: {
-      "title": 'Hi, Thanks for joining the befriend',
-      "message": "Thanks for joining befriend dashboard, submit your login ID to see the statistics and ratings that were given to you!",
+      "title" :"A message from befriend O'bot",
+      "message": "Thanks for joining befriend. This is a place where professionals like you and customers meet together.",
      "url": "https://www.google.com" 
     }
   });
   });
-  //many things are there and I can't remeber for which purpose I used this for!
-
-  //So, this is just to set subscription to true
+  var x12;
   OneSignal.setSubscription(true);
-//this is to get the userId of current user who is using
-  OneSignal.getUserId(function(userId)
+  OneSignal.getUserId(function(userId,x12)
     {      
-      //this will send the userId to thunkable
+      if(x12 == 1){
+        console.log(x12);
+        x12 = 0;        
+      }else{      
       ThunkableWebviewerExtension.postMessage(userId);
-    });
-    //this will trigger if the server recieves a message
-    ThunkableWebviewerExtension.receiveMessage(function(message) {
-
+      }
+    });  
+    ThunkableWebviewerExtension.receiveMessage(function(message,x12) {
+x12 = 1;
+if(message == null){
+  console.log(message);
+}else{
     var ty = localStorage.getItem(message);
-  // if the user is not registered then it will register   
       if(ty == null){
       OneSignal.getUserId(function(userId){    
       localStorage.setItem(message,userId);
       });
     }else{
-      // if the user is registered then it will send notification
+      function sendMessage ()
+     {         
+    var xpy =  localStorage.getItem(message);
+     OneSignal.getUserId(function() {                    
+          web_buttons=[
+           {
+           "id":"Yes",
+           "text":"I am willing" 
+           },
+           {
+             "id":"NO",
+             "text":"I am not willing"
+           }]                 
+          const body = {    
+           "app_id": "2e7a2add-017c-4365-8d3c-9ffe6b62582f",
+           "include_player_ids":[xpy],
+           "channel_for_external_user_ids": "push",
+           "data": {"foo": "bar"},           
+           "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},
+           "web_buttons": web_buttons
+          }           
+          fetch('https://onesignal.com/api/v1/notifications', {
+                  method: 'POST',
+                 body: JSON.stringify(body),
+                  headers: {               
+                     'Content-type': 'application/json; charset=UTF-8',
+                     'Authorization': 'Basic YzE0ZDk1NzYtYTExNS00YmMzLTk4ZWItMWFlOTNiNzA5NTJm'}
+             }).then(response => response.json())
+              .then(json => {
+                 console.log(json);
+              });
+            });
+          }
       sendMessage();
       OneSignal.push(["addListenerForNotificationOpened", function(event) {       
         if (event.action === 'Yes') { 
@@ -46,35 +80,5 @@ OneSignal.push(function() {
           alert("Thanks for your immediate reply");                
         }
       }]);
-      function sendMessage ()
-      {         
-     var xpy =  localStorage.getItem(message);
-      OneSignal.getUserId(function() {                    
-           web_buttons=[
-            {
-            "id":"Yes",
-            "text":"I am willing" 
-            },
-            {
-              "id":"NO",
-              "text":"I am not willing"
-            }]                 
-           const body = {    
-            "app_id": "2e7a2add-017c-4365-8d3c-9ffe6b62582f",
-            "include_player_ids":[xpy],
-            "channel_for_external_user_ids": "push",
-            "data": {"foo": "bar"},
-            "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},
-            "web_buttons": web_buttons
-           }           
-           fetch('https://onesignal.com/api/v1/notifications', {
-                   method: 'POST',
-                  body: JSON.stringify(body),
-                   headers: {               
-                      'Content-type': 'application/json; charset=UTF-8',
-                      'Authorization': 'Basic YzE0ZDk1NzYtYTExNS00YmMzLTk4ZWItMWFlOTNiNzA5NTJm'}
-              }).then(response => response.json())
-               .then(json => {
-                  console.log(json);
-               });})}}});   
+    }}});
     
