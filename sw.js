04ab07cdda2bc86f353f2571846ba9e2c929a9d6
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-11-13 10:47:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,50 +22,6 @@ function render(x12)
       console.log(x12);
     }     
       else{      
-      ThunkableWebviewerExtension.postMessage(userId);
-      sendMessage();
+      ThunkableWebviewerExtension.postMessage(userId);      
       }
-    });}           
-function sendMessage()
-  {     
-        ThunkableWebviewerExtension.receiveMessage(function(message,x12) {      
-          x12 = 1;
-          if(message == null){
-            console.log(message);
-          }else{  
-            OneSignal.getUserId(function() {                    
-              web_buttons=[
-               {
-               "id":"Yes",
-               "text":"I am willing" 
-               },
-               {
-                 "id":"NO",
-                 "text":"I am not willing"
-               }]                 
-              const body = {    
-               "app_id": "2e7a2add-017c-4365-8d3c-9ffe6b62582f",
-               "include_player_ids":[message],
-               "channel_for_external_user_ids": "push",
-               "data": {"foo": "bar"},           
-               "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},
-               "web_buttons": web_buttons
-              }           
-              fetch('https://onesignal.com/api/v1/notifications', {
-                      method: 'POST',
-                     body: JSON.stringify(body),
-                      headers: {               
-                         'Content-type': 'application/json; charset=UTF-8',
-                         'Authorization': 'Basic YzE0ZDk1NzYtYTExNS00YmMzLTk4ZWItMWFlOTNiNzA5NTJm'}
-                 }).then(response => response.json())
-                  .then(json => {
-                     console.log(json);
-                  });});}});}    
-      
-      OneSignal.push(["addListenerForNotificationOpened", function(event) {       
-        if (event.action === 'Yes') {       
-          ThunkableWebviewerExtension.postMessage(ty);          
-        } else if (event.action === 'No') {                 
-          alert("Thanks for your immediate reply");                
-        }
-  }]);  
+    });}        
